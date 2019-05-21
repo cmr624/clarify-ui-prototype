6a367a5dec685186ad8a99988a9e54af1025a38e
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-05-21 18:03:13 -0400

    adding question pinning

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import {Button} from '@material-ui/core';
+import {Button, TextField} from '@material-ui/core';
 import sample from './sample.pdf';
 //create react app nonsense
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -9,6 +9,8 @@ class Presentation extends React.Component {
   state = {
     numPages: null,
     pageNumber: 1,
+    currentQuestion:"What does 'memory regions' mean?",
+    questions:[],
   }
  
   prevPage = (e) => {
@@ -30,6 +32,13 @@ class Presentation extends React.Component {
   onDocumentLoadSuccess = ({ numPages }) => {
     this.setState({ numPages });
   }
+
+  onSubmitQuestion = (e) => {
+      e.preventDefault();
+      const q = {pageNumber:this.state.pageNumber, question:e.target.Question.value};
+      this.state.questions.push(q);
+      this.forceUpdate();
+  }
  
   render() {
     const { pageNumber, numPages } = this.state;
@@ -47,7 +56,33 @@ class Presentation extends React.Component {
         <Button 
         variant="contained"
         onClick={this.nextPage}>Next Page</Button>
+        <form onSubmit={this.onSubmitQuestion}>
+        <TextField
+          id="outlined-multiline-flexible"
+          multiline
+          label="Question"
+          name="Question"
+          value={this.state.currentQuestion}
+          margin="normal"
+          variant="outlined"
+        />
+        <Button 
+        variant="contained"
+        type="submit">Ask Question</Button>
+        </form>
+        <div>
+          <h4>Questions for slide {this.state.pageNumber}</h4>
+          <ul>
+            {this.state.questions.map((e) => {
+              if (e.pageNumber === this.state.pageNumber)
+              {
+                return (<li>{e.question}</li>);
+              }
+            })}
+          </ul>
+        </div>
         </>
+        
     );
   }
 }
